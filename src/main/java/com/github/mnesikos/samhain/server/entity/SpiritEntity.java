f1db67334c59508e21df005266bdbf85127a8e9a
Iker Fernández <ikerfer002@gmail.com>
2019-09-30 20:35:34 +0200

    Create SpiritEntity.java

---
+++ src/main/java/com/github/mnesikos/samhain/server/entity/SpiritEntity.java
@@ -0,0 +1,21 @@
+package com.github.mnesikos.samhain.server.entity;
+
+import net.minecraft.entity.AgeableEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+public class SpiritEntity extends AnimalEntity {
+
+    protected SpiritEntity(EntityType<? extends AnimalEntity> type, World world) {
+        super(type, world);
+    }
+
+    @Nullable
+    @Override
+    public AgeableEntity createChild(AgeableEntity ageableEntity) {
+        return null;
+    }
+}
