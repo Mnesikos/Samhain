10f31d925bde6a0d99923cfce94c463b7a58107e
Mnesikos <mnesikos@gmail.com>
2019-10-31 13:04:36 -0400

    start of portal structure redo

--- src/main/java/com/github/mnesikos/samhain/common/world/gen/feature/OtherworldPortalFeature.java
+++ src/main/java/com/github/mnesikos/samhain/common/world/gen/feature/OtherworldPortalFeature.java
@@ -21,99 +21,104 @@ public class OtherworldPortalFeature extends Feature<NoFeatureConfig> {
 
     @Override
     public boolean place(IWorld worldIn, ChunkGenerator<? extends GenerationSettings> generator, Random rand, BlockPos pos, NoFeatureConfig config) {
-        int i = rand.nextInt(3) + rand.nextInt(2) + 6;
-        int j = pos.getX();
-        int k = pos.getY();
-        int l = pos.getZ();
-        if (k >= 1 && k + i + 1 < 256) {
-            BlockPos blockpos = pos.down();
-            this.setDirtAt(worldIn, blockpos, pos);
-            this.setDirtAt(worldIn, blockpos.east(), pos);
-            this.setDirtAt(worldIn, blockpos.south(), pos);
-            this.setDirtAt(worldIn, blockpos.south().east(), pos);
-            Direction direction = Direction.Plane.HORIZONTAL.random(rand);
-            int i1 = i - rand.nextInt(4);
-            int j1 = 2 - rand.nextInt(3);
-            int k1 = j;
-            int l1 = l;
-            int i2 = k + i - 1;
-            BlockPos portalPos = blockpos.up();
-            for (int j2 = 0; j2 < i; ++j2) {
-                if (j2 >= i1 && j1 > 0) {
-                    k1 += direction.getXOffset();
-                    l1 += direction.getZOffset();
-                    --j1;
-                }
+        int height = rand.nextInt(3) + rand.nextInt(2) + 6; // 0~2 + 0~1 + 6 // max = 9, min = 6
+        int posX = pos.getX();
+        int posY = pos.getY();
+        int posZ = pos.getZ();
+        if (posY >= 1 && posY + height + 1 < 256) {
+            BlockPos base = pos.down();
+            this.create3x3(worldIn, base);
+            this.setDirtAt(worldIn, base.down(), pos);
+            this.setDirtAt(worldIn, base.down().east(), pos);
+            this.setDirtAt(worldIn, base.down().west(), pos);
+            this.setDirtAt(worldIn, base.down().north(), pos);
+            this.setDirtAt(worldIn, base.down().north().east(), pos);
+            this.setDirtAt(worldIn, base.down().north().west(), pos);
+            this.setDirtAt(worldIn, base.down().south(), pos);
+            this.setDirtAt(worldIn, base.down().south().east(), pos);
+            this.setDirtAt(worldIn, base.down().south().west(), pos);
 
-                int k2 = k + j2;
-                BlockPos blockpos1 = new BlockPos(k1, k2, l1);
-                if (isAirOrLeaves(worldIn, blockpos1)) {
-                    this.createHollow3x3(worldIn, blockpos1);
-                    this.createHollow3x3(worldIn, blockpos1.east());
-                    this.createHollow3x3(worldIn, blockpos1.south());
-                    this.createHollow3x3(worldIn, blockpos1.east().south());
+            Direction direction = Direction.Plane.HORIZONTAL.random(rand);
+            int height1 = height - rand.nextInt(4); // height - 0~3 // max = 9, min = 3
+            int j1 = 2 - rand.nextInt(3); // 2 - 0~2 // max = 2, min = 0
+            int posX1 = posX;
+            int posZ1 = posZ;
+            int i2 = posY + height - 1;
+            BlockPos portalPos = base.up();
+            for (int j2 = 0; j2 < height; ++j2) {
+                /*if (j2 >= height1 && j1 > 0) { // wtf is this?
+                    posX1 += direction.getXOffset();
+                    posZ1 += direction.getZOffset();
+                    --j1;
+                }*/
+
+                int k2 = posY + j2;
+                BlockPos blockpos1 = new BlockPos(posX1, k2, posZ1);
+                if (isAirOrLeaves(worldIn, blockpos1)) { // trunk
+                    this.create3x3(worldIn, blockpos1);
                 }
             }
 
-            for (int j3 = -2; j3 <= 0; ++j3) {
+            for (int p = 0; p < 3; p++) setBlockState(worldIn, portalPos.up(p), ModBlocks.OTHERWORLD_PORTAL.getDefaultState());
+
+            /*for (int j3 = -2; j3 <= 0; ++j3) {
                 for (int i4 = -2; i4 <= 0; ++i4) {
                     int l4 = -1;
-                    this.replaceAirWithLeaves(worldIn, k1 + j3, i2 + l4, l1 + i4);
-                    this.replaceAirWithLeaves(worldIn, 1 + k1 - j3, i2 + l4, l1 + i4);
-                    this.replaceAirWithLeaves(worldIn, k1 + j3, i2 + l4, 1 + l1 - i4);
-                    this.replaceAirWithLeaves(worldIn, 1 + k1 - j3, i2 + l4, 1 + l1 - i4);
+                    this.replaceAirWithLeaves(worldIn, posX1 + j3, i2 + l4, posZ1 + i4);
+                    this.replaceAirWithLeaves(worldIn, 1 + posX1 - j3, i2 + l4, posZ1 + i4);
+                    this.replaceAirWithLeaves(worldIn, posX1 + j3, i2 + l4, 1 + posZ1 - i4);
+                    this.replaceAirWithLeaves(worldIn, 1 + posX1 - j3, i2 + l4, 1 + posZ1 - i4);
                     if ((j3 > -2 || i4 > -1) && (j3 != -1 || i4 != -2)) {
                         l4 = 1;
-                        this.replaceAirWithLeaves(worldIn, k1 + j3, i2 + l4, l1 + i4);
-                        this.replaceAirWithLeaves(worldIn, 1 + k1 - j3, i2 + l4, l1 + i4);
-                        this.replaceAirWithLeaves(worldIn, k1 + j3, i2 + l4, 1 + l1 - i4);
-                        this.replaceAirWithLeaves(worldIn, 1 + k1 - j3, i2 + l4, 1 + l1 - i4);
+                        this.replaceAirWithLeaves(worldIn, posX1 + j3, i2 + l4, posZ1 + i4);
+                        this.replaceAirWithLeaves(worldIn, 1 + posX1 - j3, i2 + l4, posZ1 + i4);
+                        this.replaceAirWithLeaves(worldIn, posX1 + j3, i2 + l4, 1 + posZ1 - i4);
+                        this.replaceAirWithLeaves(worldIn, 1 + posX1 - j3, i2 + l4, 1 + posZ1 - i4);
                     }
                 }
-            }
+            }*/
 
-            if (rand.nextBoolean()) {
-                this.replaceAirWithLeaves(worldIn, k1, i2 + 2, l1);
-                this.replaceAirWithLeaves(worldIn, k1 + 1, i2 + 2, l1);
-                this.replaceAirWithLeaves(worldIn, k1 + 1, i2 + 2, l1 + 1);
-                this.replaceAirWithLeaves(worldIn, k1, i2 + 2, l1 + 1);
-            }
+            /*if (rand.nextBoolean()) {
+                this.replaceAirWithLeaves(worldIn, posX1, i2 + 2, posZ1);
+                this.replaceAirWithLeaves(worldIn, posX1 + 1, i2 + 2, posZ1);
+                this.replaceAirWithLeaves(worldIn, posX1 + 1, i2 + 2, posZ1 + 1);
+                this.replaceAirWithLeaves(worldIn, posX1, i2 + 2, posZ1 + 1);
+            }*/
 
-            for (int k3 = -3; k3 <= 4; ++k3) {
+            /*for (int k3 = -3; k3 <= 4; ++k3) {
                 for (int j4 = -3; j4 <= 4; ++j4) {
                     if ((k3 != -3 || j4 != -3) && (k3 != -3 || j4 != 4) && (k3 != 4 || j4 != -3) && (k3 != 4 || j4 != 4) && (Math.abs(k3) < 3 || Math.abs(j4) < 3)) {
-                        this.replaceAirWithLeaves(worldIn, k1 + k3, i2, l1 + j4);
+                        this.replaceAirWithLeaves(worldIn, posX1 + k3, i2, posZ1 + j4);
                     }
                 }
-            }
+            }*/
 
-            for (int l3 = -1; l3 <= 2; ++l3) {
+            /*for (int l3 = -1; l3 <= 2; ++l3) {
                 for (int k4 = -1; k4 <= 2; ++k4) {
                     if ((l3 < 0 || l3 > 1 || k4 < 0 || k4 > 1) && rand.nextInt(3) <= 0) {
                         int i5 = rand.nextInt(3) + 2;
 
                         for (int l2 = 0; l2 < i5; ++l2) {
-                            this.createHollow3x3(worldIn, new BlockPos(j + l3, i2 - l2 - 1, l + k4));
+                            this.create3x3(worldIn, new BlockPos(posX + l3, i2 - l2 - 1, posZ + k4));
                         }
 
                         for (int j5 = -1; j5 <= 1; ++j5) {
                             for (int i3 = -1; i3 <= 1; ++i3) {
-                                this.replaceAirWithLeaves(worldIn, k1 + l3 + j5, i2, l1 + k4 + i3);
+                                this.replaceAirWithLeaves(worldIn, posX1 + l3 + j5, i2, posZ1 + k4 + i3);
                             }
                         }
 
                         for (int k5 = -2; k5 <= 2; ++k5) {
                             for (int l5 = -2; l5 <= 2; ++l5) {
                                 if (Math.abs(k5) != 2 || Math.abs(l5) != 2) {
-                                    this.replaceAirWithLeaves(worldIn, k1 + l3 + k5, i2 - 1, l1 + k4 + l5);
+                                    this.replaceAirWithLeaves(worldIn, posX1 + l3 + k5, i2 - 1, posZ1 + k4 + l5);
                                 }
                             }
                         }
-                        setBlockState(worldIn, portalPos, ModBlocks.OTHERWORLD_PORTAL.getDefaultState());
-                        for (BlockPos p = portalPos.up(); !worldIn.getBlockState(p).isAir(worldIn, p); p = p.up()) setBlockState(worldIn, p, Blocks.CAVE_AIR.getDefaultState());
+                        // portal was here
                     }
                 }
-            }
+            }*/
 
             return true;
 
@@ -122,8 +127,8 @@ public class OtherworldPortalFeature extends Feature<NoFeatureConfig> {
         }
     }
 
-    private void createHollow3x3(IWorldWriter worldIn, BlockPos pos) {
-        this.setBlockState(worldIn, pos, Blocks.CAVE_AIR.getDefaultState());
+    private void create3x3(IWorldWriter worldIn, BlockPos pos) {
+        this.setBlockState(worldIn, pos, CrimsonKingMapleTreeFeature.LOG);
         this.setBlockState(worldIn, pos.north(), CrimsonKingMapleTreeFeature.LOG);
         this.setBlockState(worldIn, pos.south(), CrimsonKingMapleTreeFeature.LOG);
         this.setBlockState(worldIn, pos.east(), CrimsonKingMapleTreeFeature.LOG);
